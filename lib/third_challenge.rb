e0968f3d656009fbae53e6271280b1bcbd55bee9
Yani Asari <yani.iman1@gmail.com>
2020-08-09 02:00:08 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
       :hero_friends => []}, 
    :capulet => {:patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliette", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []}
   }
   
