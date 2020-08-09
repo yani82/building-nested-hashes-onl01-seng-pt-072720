5e887bc4315b941ed0a8b9fc9bf16631e9b9f74b
Yani Asari <yani.iman1@gmail.com>
2020-08-09 01:39:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -21,7 +21,8 @@ def third_challenge
         name: "Lord Capulet",
         age: "50"
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
