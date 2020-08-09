3be2a8643f7e4dc1f12a9b7d4a34bd117d20a370
Yani Asari <yani.iman1@gmail.com>
2020-08-09 01:49:02 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,11 +1,7 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => []
-   }, 
+   :montague => {:patriarch => {name: "Lord Montague", age: "53"}, :matriarch => {name: "Lady Montague", age: "54"}, :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => []}, 
    :capulet => {
       :patriarch => {
         name: "Lord Capulet",
